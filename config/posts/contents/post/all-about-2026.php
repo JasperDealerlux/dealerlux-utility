7c767fa815f29674e7ab76c0381e801af20b470c
jasperjardin <jasper.jardin1994@gmail.com>
2026-07-24 14:18:48 +0800

    feat: add All About 2026 post content to virtual posts registry

---
+++ config/posts/contents/post/all-about-2026.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * All About 2026 post content.
+ *
+ * @package DealerluxUtils
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+return <<<'HTML'
+<!-- wp:heading -->
+<h2 class="wp-block-heading">All About 2026</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>This content is compiled into the virtual posts registry cache.</p>
+<!-- /wp:paragraph -->
+HTML;

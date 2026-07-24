8a67ef33d8500e8efe375f2d9e318441ce40b3f1
jasperjardin <jasper.jardin1994@gmail.com>
2026-07-24 14:18:18 +0800

    feat: add virtual posts definitions for 'All About 2026'

---
+++ config/posts/types/post.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Virtual posts definitions.
+ *
+ * @package DealerluxUtils
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+return array(
+	'all-about-2026' => array(
+		'slug'         => 'all-about-2026',
+		'title'        => 'All About 2026',
+		'content_file' => 'all-about-2026.php',
+		'status'       => 'publish',
+	)
+);

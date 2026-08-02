fabaa9a8052686322d7e75a14daa014e86ca8dd1
jasperjardin <jasper.jardin1994@gmail.com>
2026-08-02 14:27:08 +0800

    feat: add initial styles and scripts for Dealerlux Forms virtual pages

---
+++ assets/posts/page/dealerlux-forms/script.js
@@ -0,0 +1,11 @@
+/**
+ * Dealerlux Forms virtual page scripts.
+ */
+(function () {
+	'use strict';
+
+	document.addEventListener('DOMContentLoaded', function () {
+		console.log('Dealerlux Forms virtual page script loaded!');
+	});
+    
+})();
